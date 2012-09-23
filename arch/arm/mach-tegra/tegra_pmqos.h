37a6b2efa0728eb8d9d2f015675c43a465211a6d
Dennis Rassmann <showp1984@gmail.com>
2012-09-24 00:13:29 +0200

    mach-tegra: fix Variant 0 OC
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>

--- arch/arm/mach-tegra/tegra_pmqos.h
+++ arch/arm/mach-tegra/tegra_pmqos.h
@@ -6,6 +6,11 @@
  * Author:
  *	faux123 <reioux@gmail.com>
  *
+ * History:
+ *      -original version (Paul Reioux)
+ *      -cleaned since oc was reworked (Dennis Rassmann)
+ *      -added comment for T3_VARIANT_BOOST (Dennis Rassmann)
+ *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
  * may be copied, distributed, and modified under those terms.
@@ -23,9 +28,11 @@
 
 #define CPUFREQ_SCALING_MAX_FREQ	"BOOST_CPU_FREQ_MIN"
 
-/* in 10 MHz */
-#define T3_VARIANT_BOOSTX	20
-#define T3_VARIANT_BOOST0	10
+/* in 10 MHz
+ * this effectively makes all phone 1700Mhz capable, but is limited by
+ * the clock definitions in tegra_dvfs.c */
+#define T3_VARIANT_BOOSTX       20
+#define T3_VARIANT_BOOST0       10
 
 extern unsigned int tegra_pmqos_boost_freq;
 extern unsigned int tegra_pmqos_cap_freq;
